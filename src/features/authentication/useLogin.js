ab7ff2a42f6b2cef0497ec2bb6b037efe76f9d1f
Han Zhang <61225353+Hannah-Zhang-H@users.noreply.github.com>
2024-08-22 11:52:44 +1200

    Fixed a bug: avoid repeated requests for user information

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,14 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { login as loginApi } from "../../services/apiAuth";
 import toast from "react-hot-toast";
 
 export function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: () => {
+    onSuccess: (user) => {
+      /*After a user successfully logs in, the latest user information can be used immediately in other pages
+      / or components. This is done to avoid repeated requests for user information */
+      queryClient.setQueryData(["user"], user.user);
       navigate("/", { replace: true });
     },
     onError: (err) => {
