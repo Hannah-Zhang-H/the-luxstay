8056957e489c2bdb1b66793caa2561db67b3ab31
Han Zhang <61225353+Hannah-Zhang-H@users.noreply.github.com>
2024-08-26 19:40:49 +1200

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint],
+  plugins: [react(), process.env.NODE_ENV !== "production" && eslint()].filter(
+    Boolean
+  ),
 });
