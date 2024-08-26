63443f3a4670d5b00a7a0822388c79263c380107
Han Zhang <61225353+Hannah-Zhang-H@users.noreply.github.com>
2024-08-26 19:07:43 +1200

    fix bugs

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -15,7 +15,7 @@ const FullPage = styled.div`
 function ProtectedRoute({ children }) {
   const navigate = useNavigate();
   // 1. Load the authenticated user
-  const { isLoading, user, isAuthenticated } = useUser();
+  const { isLoading, isAuthenticated } = useUser();
 
   // 2. If there is NO authenticated user, redirect to the log in page
   useEffect(() => {
