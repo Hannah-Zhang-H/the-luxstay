f6c256fd8a63c16b918548f189c9e5e7fced4f37
Han Zhang <61225353+Hannah-Zhang-H@users.noreply.github.com>
2024-08-26 19:28:20 +1200

    update

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
-import { useUser } from "../features/authentication/useUser.js";
+
 import Spinner from "./Spinner";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import { useUser } from "../features/authentication/useUser";
 
 const FullPage = styled.div`
   height: 100vh;
