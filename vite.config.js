9aff3a7fbd80dc2d6063339d0a92ffdcf8b255a0
Han Zhang <61225353+Hannah-Zhang-H@users.noreply.github.com>
2024-08-26 22:47:07 +1200

    change vite.config file

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), process.env.NODE_ENV !== "production" && eslint()].filter(
-    Boolean
-  ),
+  plugins: [react(), eslint],
 });
